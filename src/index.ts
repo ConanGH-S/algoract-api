b09952c122b17d3ca0873868dd870d0b7be5a1bc
ConanGH-S <blandon0207s@gmail.com>
2024-04-03 18:22:27 -0500

    remove auto routes and add login route

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
-import { Elysia } from 'elysia'
-import { autoroutes } from 'elysia-autoroutes'
+import { Elysia, t } from 'elysia'
+import { jwt } from '@elysiajs/jwt'
 
-const app = new Elysia()
+const app = new Elysia({ prefix: '/api' })
   .use(
-    autoroutes({
-      routesDir: './routes',
-      prefix: '/api',
-      generateTags: false
-    })
+    jwt({ name: 'jwt', secret: 'john doe' })
   )
-  .listen(3000)
+  .post('/login',
+    async ({ jwt, body: { username, password }, cookie: { auth } }) => {
+      auth.set({
+        value: await jwt.sign({ username, password }),
+        httpOnly: true,
+        maxAge: 7 * 86400,
+        path: '/',
+        sameSite: 'lax'
+      })
+    }, {
+      body: t.Object({
+        username: t.String({ minLength: 3 }),
+        password: t.String({ minLength: 8, pattern: '^(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$' })
+      })
+    })
+  .listen(3000, ({ port, url }) => {
+    console.log(`Server listening on port ${port}`)
+  })
 
 export type ElysiaApp = typeof app
