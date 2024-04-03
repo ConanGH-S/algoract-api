b0c9f92c53b5cc83987da6b269c10c5ea17ae2ef
ConanGH-S <blandon0207s@gmail.com>
2024-04-03 17:38:48 -0500

    Project created

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get('/', () => 'Hello Elysia')
+  .get('/login', () => ({ name: 'John Doe', age: 22 }))
+  .onError(({ code }) => {
+    return code === 'NOT_FOUND' && 'Route not found'
+  })
+  .listen(3000)
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+  `Server is running at ${app.server?.hostname}:${app.server?.port}`
+)
