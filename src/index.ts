dbbe095c4182f3651dd1e5f0b0d3bf475c73d8c9
ConanGH-S <blandon0207s@gmail.com>
2024-04-03 17:42:53 -0500

    auto routes added

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import { Elysia } from 'elysia'
+import { autoroutes } from 'elysia-autoroutes'
 
 const app = new Elysia()
-  .get('/', () => 'Hello Elysia')
-  .get('/login', () => ({ name: 'John Doe', age: 22 }))
-  .onError(({ code }) => {
-    return code === 'NOT_FOUND' && 'Route not found'
-  })
+  .use(
+    autoroutes({
+      routesDir: './routes',
+      prefix: '/api',
+      generateTags: false
+    })
+  )
   .listen(3000)
 
-console.log(
-  `Server is running at ${app.server?.hostname}:${app.server?.port}`
-)
+export type ElysiaApp = typeof app
